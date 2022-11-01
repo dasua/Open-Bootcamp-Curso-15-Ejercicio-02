3705cff43dd87f3ffdcd64737f0776470eb73b24
Jesús Guerreiro Real de Asua <jesus@jesusguerreiro.es>
2022-11-01 13:23:58 +0100

    Creado script con los datos solicitados en el ejercicio
    Signed-off-by: Jesús Guerreiro Real de Asua <jesus@jesusguerreiro.es>

---
+++ index.js
@@ -0,0 +1,14 @@
+const datos = [
+    "Jesús",
+    49,
+    true,
+    new Date(1973,6,21),
+    {
+        titulo: "Vivir la vida con sentido",
+        autor: "Víctor Küppers",
+        fecha: new Date('2016-05-01Z'),
+        url: "https://www.amazon.es/Vivir-Vida-Sentido-Nueva-Edici%C3%B3n/dp/8416620792/ref=sr_1_1?qid=1667304899&refinements=p_27%3AVictor+K%C3%BCppers&s=books&sr=1-1"
+    },
+];
+
+console.log(datos);
